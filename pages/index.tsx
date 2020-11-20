2e625f4c2196dcece9690d8a36a85732882bab61
Tsuyoshi Maeda <oasis5tsuyoshi@gmail.com>
2020-11-20 14:12:38 +0900

    Setup config for typescript and tailwind.

---
+++ pages/index.tsx
@@ -0,0 +1,18 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <header>Header</header>
+
+      <main>Main</main>
+
+      <footer>Footer</footer>
+    </div>
+  );
+}
