e6cce681e62ba63b5d1a3f81894061d3cb67ba99
Tsuyoshi Maeda <oasis5tsuyoshi@gmail.com>
2020-11-21 15:46:08 +0900

    Moved Card.stories to Component

--- src/stories/Components/Card.stories.tsx
+++ src/stories/Components/Card.stories.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 // also exported from '@storybook/react' if you can deal with breaking changes in 6.1
 import { Story, Meta } from '@storybook/react/types-6-0';
 
-import Card, { Props as CardProps } from '../components/Card';
+import Card, { Props as CardProps } from '../../components/Card';
 
 export default {
-  title: 'Example/Card',
+  title: 'Components/Card',
   component: Card,
   argTypes: {
     backgroundColor: { control: 'color' },
