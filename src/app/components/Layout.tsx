a81a51994851675d6852c3eea65435ea3e216d1e
Tsuyoshi Maeda <oasis5tsuyoshi@gmail.com>
2020-11-25 20:22:06 +0900

    Set meta info.

--- src/app/components/Layout.tsx
+++ src/app/components/Layout.tsx
@@ -1,10 +1,26 @@
+import Head from 'next/head';
 import AppBar from 'components/AppBar';
 import DeviceIndicatorBySize from 'components/development/DeviceIndicatorBySize';
 import { ToastContainer } from 'react-toastify';
 
 export default function Layout({ children }) {
+  const title = 'My質問回答サービス';
+  const description = '質問と回答を行えるサービスです。';
+
   return (
     <div className="bg-gray-300 min-h-screen">
+      <Head>
+        <title>{title}</title>
+        <meta name="description" key="description" content={description} />
+        <meta property="og:title" key="ogTItle" content={title} />
+        <meta property="og:site_name" key="ogSiteName" content={title} />
+        <meta
+          property="og:description"
+          key="ogDescription"
+          content={description}
+        />
+      </Head>
+
       {process.env.NODE_ENV === 'development' ? (
         <DeviceIndicatorBySize />
       ) : null}
